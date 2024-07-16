068b8f9fd6f4fa463daaa365aba9df729439cb1c
Roshani-Poojari <roshanipoojari2002@gmail.com>
2024-07-16 18:34:35 +0530

    first commit

---
+++ Days.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDemo
+{
+    internal enum Days
+    {
+        Monday = 1,
+        //Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Days of a week : \n{(int)Days.Monday} {Days.Monday}\n{(int)Days.Tuesday} {Days.Tuesday}\n"+
+                $"{(int)Days.Wednesday} {Days.Wednesday}\n{(int)Days.Thursday} {Days.Thursday}\n"+
+                $"{(int)Days.Friday} {Days.Friday}\n{(int)Days.Saturday} {Days.Saturday}\n{(int)Days.Sunday} {Days.Sunday}");
+        }
+    }
+}
